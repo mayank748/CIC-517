5c931238a8d3b73d041cb87bfdfa9a935549c196
Mayank <smayank748@gmail.com>
2022-05-18 08:35:07 +0530

    [Mayank]syntext for case statement in shell script

--- firstShell.sh
+++ firstShell.sh
@@ -1,21 +1,16 @@
 #! /bin/bash -x
 
-#for loop
-for file in `ls *.txt`
-do
-	echo $file
-done
-
-for((i=0;i<10;i++))
-do
-	echo $i
-done
-
-
-#while loop
-count=0
-while [ $count -le 5 ]
-do
-	echo $count
-	((count++))
-done
+case pattern in 
+	1)
+		#body
+		;;
+	1)
+		#body
+		;;
+	""
+	""
+	""
+	*)
+		#body
+		;;
+esac
