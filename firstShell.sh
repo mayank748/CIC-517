604b0706a3daaf1a7c69f63ed4daaa2f23d28df2
Mayank <smayank748@gmail.com>
2022-05-19 08:53:35 +0530

    [Mayank]worked on arrays

--- firstShell.sh
+++ firstShell.sh
@@ -1,21 +1,23 @@
 #! /bin/bash -x
+declare -a myArray
+myArray[0]="first value"
+myArray[1]="Second value"
+myArray[2]="third value"
+myArray[3]="fourth value"
+checkValue=0;
 
-#for loop
-for file in `ls *.txt`
+echo "all the value of array " ${myArray[@]}
+echo "length of array " ${#myArray[@]}
+echo "to get specific array value " ${myArray[2]}
+
+value=''
+for((i=0;i<${#myArray[@]};i++))
 do
-	echo $file
+	if [ $i -lt ${#myArray[@]} -a $i -ne 0 ]
+	then
+		value=$value" , "${myArray[$i]}
+	else
+		value=$value" "${myArray[$i]}
+	fi
 done
-
-for((i=0;i<10;i++))
-do
-	echo $i
-done
-
-
-#while loop
-count=0
-while [ $count -le 5 ]
-do
-	echo $count
-	((count++))
-done
+echo $value
